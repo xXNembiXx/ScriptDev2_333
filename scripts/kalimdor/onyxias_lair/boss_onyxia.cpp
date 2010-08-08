c91f38f6fdcdbcbf059da56d479030a53da09c9e
Janu <jaed1@gmx.net>
2010-08-09 00:31:43 +0200

    corrected "[S41] some hacks in onyxia script"
    Signed-off-by: Janu <jaed1@gmx.net>

--- scripts/kalimdor/onyxias_lair/boss_onyxia.cpp
+++ scripts/kalimdor/onyxias_lair/boss_onyxia.cpp
@@ -279,7 +279,7 @@ struct MANGOS_DLL_DECL boss_onyxiaAI : public ScriptedAI
 
         if (m_uiEvadeCheckCooldown < uiDiff)
         {
-            if (m_creature->GetDistance2d(-128.236f, -214.887f) > 80.0f)
+            if (m_creature->GetDistance2d(-22,3467f, -214,571f) > 100.0f)
                 EnterEvadeMode();
             m_uiEvadeCheckCooldown = 2000;
         }

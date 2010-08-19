7593ed05e77f7d3f37f779dc6ee4997a53e087a4
Janu <jaed1@gmx.net>
2010-08-19 15:48:38 +0200

    some changes in SQL
    Signed-off-by: Janu <jaed1@gmx.net>

--- Custom/[S44] Deadmines_Mr_Smite_mangos.sql
+++ Custom/[S44] Deadmines_Mr_Smite_mangos.sql
@@ -1,5 +1,4 @@
+-- script for Mr. Smite in instance Deadmines
 
-
--- Scriptname
 DELETE FROM `creature_ai_scripts` WHERE `creature_id`='646';
 UPDATE `creature_template` SET `AIName`='',`ScriptName`='boss_mrsmite' WHERE (`entry`='646');

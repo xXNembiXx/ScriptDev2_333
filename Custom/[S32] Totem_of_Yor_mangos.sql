450ef9842601aa45d0a8ae381ca6489ac7f7ef01
Ancient <Phil@.(none)>
2010-09-20 23:09:42 +0200

    [S78] addition to "[S32] Totem_of_Yor_mangos.sql"
    Signed-off-by: Janu <jaed1@gmx.net>

--- Custom/[S32] Totem_of_Yor_mangos.sql
+++ Custom/[S32] Totem_of_Yor_mangos.sql
@@ -2,5 +2,6 @@ DELETE FROM `creature` WHERE (`id`='17392');
 UPDATE `creature_template` SET `ScriptName`='npc_ancestor_tikti' WHERE (`entry`='17392');
 UPDATE `creature_template` SET `ScriptName`='npc_totem_of_tikti' WHERE (`entry`='17362');
 UPDATE `quest_template` SET `SpecialFlags`='2', `SrcSpell`=NULL WHERE (`entry`='9541');
-
+DELETE FROM `creature` WHERE `entry`='17410';
+DELETE FROM `creature_ai_scripts` WHERE `creature_id`='17393';
 
